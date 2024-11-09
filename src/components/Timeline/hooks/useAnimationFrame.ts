82eee7b88efed09124e39d779e056464a582d55f
type9 <gabespersonal@gmail.com>
2024-11-09 21:21:26 +0800

    chore: adds comments on notes about animation performance

--- src/components/Timeline/hooks/useAnimationFrame.ts
+++ src/components/Timeline/hooks/useAnimationFrame.ts
@@ -3,8 +3,10 @@ import { useRef, useCallback, useEffect } from 'react';
 /*
  * This hook is used to schedule animation frames for intensive updates and immediate updates.
  * It is used to prevent the browser from performing unnecessary repaints and also can sync scrolls and drags immediately rather than waiting an additional frame.
- * It also supposedly implicitly throttles since its limited to the browser's fps.
+ * It also supposedly implicitly throttles since its limited to the browser's vsync fps.
  * NOTE: This is a naive implementation. Simultaneous animation frames will nullify the oldest one. This only works if there is only one animation frame running at a time.
+ * ALSO NOTE: I Noticed that event in the demo provided horizontal scroll still has a small lag on the other div. Reading documentation would lead you to believe that it should be perfectly in sync.
+ * My guess this has to do with that scroll is a consequence of wheel, keydown, touchmove, etc. So unless we listen and hijack all those events, there is no way to render all in sync.
  */
 export const useAnimationFrame = () => {
   const animationFrameIdRef = useRef<number | null>(null);
